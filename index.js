7fe7e4426f198cf38497ebacbdc814c7a7ae462f
MeganViga <viganesh.sm@gmail.com>
2024-06-14 16:47:27 +0530

    Added dot env support

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+import 'dotenv/config'
 import express from "express"
 const app = express()
-const port = 9000
+const port = process.env.PORT || 9000
 app.get("/",(req, res)=>{
     res.send("Hello from viganesh and his tea!")
 })
